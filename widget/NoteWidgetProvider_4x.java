846220045fbe90bc34189491c6407ae5a2a9b9ba
qmy233 <1692579426@qq.com>
2025-03-31 21:11:10 +0800

    Update NoteWidgetProvider_4x.java

--- widget/NoteWidgetProvider_4x.java
+++ widget/NoteWidgetProvider_4x.java
@@ -3,12 +3,15 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * 您可以在以下网址获取许可证副本：
+ * You may obtain a copy of the License at
  *
  *        http://www.apache.org/licenses/LICENSE-2.0
  *
- * 除非法律要求或书面同意，本软件按"原样"提供，无任何形式的担保。
- * 详见许可证对权限和限制的具体规定。
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package net.micode.notes.widget;
